e84d42b12b9152fd30c4616f57ba929b58a3e177
nshoron <119867650+nshoron@users.noreply.github.com>
2025-05-05 02:15:21 +0600

    Add files via upload

--- process.php
+++ process.php
@@ -5,22 +5,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $_POST["password"];
     $confirmPassword = $_POST["confirm-password"];
     $dob = $_POST["dob"];
-    $country = $_POST["country"];
+    $city = $_POST["city"];
     $gender = isset($_POST["gender"]) ? $_POST["gender"] : 'Not specified';
     $zipcode = $_POST["zipcode"];
     $terms = isset($_POST["terms"]) ? "Agreed" : "Not Agreed";
 
-    echo "<div style='margin-top: 20px; padding: 15px; border: 1px solid #000; background-color: #e0ffe0;'>";
+    echo "<div style='margin-top: 20px; padding: 15px; border: 1px solid #000; background-color:rgb(68, 156, 68);'>";
     echo "<h3>Submitted Registration Details</h3>";
     echo "<p><strong>Full Name:</strong> $fullname</p>";
     echo "<p><strong>Email:</strong> $email</p>";
-    echo "<p><strong>Password:</strong> $password</p>";
-    echo "<p><strong>Confirm Password:</strong> $confirmPassword</p>";
     echo "<p><strong>Date of Birth:</strong> $dob</p>";
-    echo "<p><strong>Country:</strong> $country</p>";
+    echo "<p><strong>City:</strong> $city</p>";
     echo "<p><strong>Gender:</strong> $gender</p>";
     echo "<p><strong>ZIP Code:</strong> $zipcode</p>";
-    echo "<p><strong>Terms:</strong> $terms</p>";
     echo "</div>";
 }
 ?>
